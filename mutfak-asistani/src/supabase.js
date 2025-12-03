f8d6ccf6e091ed209c9fce26c06550597357e28b
ezltv <ez41tv@gmail.com>
2025-12-03 14:27:37 +0000

    senelere

--- mutfak-asistani/src/supabase.js
+++ mutfak-asistani/src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rgfbelamzofpookkfsqr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnZmJlbGFtem9mcG9va2tmc3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NTA5MDEsImV4cCI6MjA4MDMyNjkwMX0.xvvoFKQMvHGzpoc4ZIkhGY7FKcMKaHJl6_SQasMXRvY'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL = 'https://rgfbelamzofpookkfsqr.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnZmJlbGFtem9mcG9va2tmc3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NTA5MDEsImV4cCI6MjA4MDMyNjkwMX0.xvvoFKQMvHGzpoc4ZIkhGY7FKcMKaHJl6_SQasMXRvY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
